04b8933a1e5497c0dfa69eaeb7c8ea6bf92f62ec
Kiss Norbert <nn@norbilaptop.lan>
2018-09-27 19:15:41 +0200

    some development

--- src/passwordgenerator/PasswordGenerator.java
+++ src/passwordgenerator/PasswordGenerator.java
@@ -12,6 +12,15 @@ public class PasswordGenerator {
     private boolean allowedUpper;
     private boolean allowedDigit;
     private boolean allowedPunctuation;
+    private int minNumberLower;
+    private int minNumberUpper;
+    private int minNumberDigit;
+    private int minNumberPunctuation;
+
+    private StringBuffer lowers;
+    private StringBuffer uppers;
+    private StringBuffer digits;
+    private StringBuffer punctuations;
 
     public PasswordGenerator() {
         this(false);
@@ -26,38 +35,78 @@ public class PasswordGenerator {
         this.allowedUpper = allowedUpper;
         this.allowedDigit = allowedDigit;
         this.allowedPunctuation = allowedPunctuation;
+
+        this.minNumberLower = 0;
+        this.minNumberUpper = 0;
+        this.minNumberDigit = 0;
+        this.minNumberPunctuation = 0;
+
+        this.lowers = new StringBuffer();
+        this.uppers = new StringBuffer();
+        this.digits = new StringBuffer();
+        this.punctuations = new StringBuffer();
     }
 
     public void enableLower() {
-        allowedLower = true;
+        this.allowedLower = true;
     }
 
     public void disableLower() {
-        allowedLower = false;
+        this.allowedLower = false;
     }
 
     public void enableUpper() {
-        allowedUpper = true;
+        this.allowedUpper = true;
     }
 
     public void disableUpper() {
-        allowedUpper = false;
+        this.allowedUpper = false;
     }
 
     public void enableDigit() {
-        allowedDigit = true;
+        this.allowedDigit = true;
     }
 
     public void disableDigit() {
-        allowedDigit = false;
+        this.allowedDigit = false;
     }
 
     public void enablePunctuation() {
-        allowedPunctuation = true;
+        this.allowedPunctuation = true;
     }
 
     public void disablePunctuation() {
-        allowedPunctuation = false;
+        this.allowedPunctuation = false;
+    }
+
+    public void setEnabledCharacters(boolean allowedLower, boolean allowedUpper, boolean allowedDigit, boolean allowedPunctuation) {
+        this.allowedLower = allowedLower;
+        this.allowedUpper = allowedUpper;
+        this.allowedDigit = allowedDigit;
+        this.allowedPunctuation = allowedPunctuation;
+    }
+
+    public void setMinNumberLower(int minNumberLower) {
+        this.minNumberLower = minNumberLower;
+    }
+
+    public void setMinNumberUpper(int minNumberUpper) {
+        this.minNumberUpper = minNumberUpper;
+    }
+
+    public void setMinNumberDigit(int minNumberDigit) {
+        this.minNumberDigit = minNumberDigit;
+    }
+
+    public void setMinNumberPunctuation(int minNumberPunctuation) {
+        this.minNumberPunctuation = minNumberPunctuation;
+    }
+
+    public void setMinNumberCharacters(int minNumberLower, int minNumberUpper, int minNumberDigit, int minNumberPunctuation) {
+        this.minNumberLower = minNumberLower;
+        this.minNumberUpper = minNumberUpper;
+        this.minNumberDigit = minNumberDigit;
+        this.minNumberPunctuation = minNumberPunctuation;
     }
 
     public String generate() {
@@ -65,17 +114,68 @@ public class PasswordGenerator {
     }
 
     public String generate(int length) {
-        String password = "";
+        String password = getMinChars();
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < (length - password.length()); i++) {
             password += getNextChar();
         }
+
+        StringBuffer sb = new StringBuffer(password);
+        // shuffle strings
+        sb.s
 
         return password;
     }
 
+    private String getMinChars() {
+        generateLowers();
+        generateUppers();
+        generateDigits();
+        generatePunctuations();
+
+        StringBuffer minChars = new StringBuffer();
+        minChars.append(this.lowers);
+        minChars.append(this.uppers);
+        minChars.append(this.digits);
+        minChars.append(this.punctuations);
+
+        return minChars.toString();
+    }
+
+    private void generateLowers() {
+        if (this.allowedLower) {
+            for (int i = 0; i < this.minNumberLower; i++) {
+                this.lowers.append(getNextLowerChar());
+            }
+        }
+    }
+
+    private void generateUppers() {
+        if (this.allowedUpper) {
+            for (int i = 0; i < this.minNumberUpper; i++) {
+                this.uppers.append(getNextUpperChar());
+            }
+        }
+    }
+
+    private void generateDigits() {
+        if (this.allowedDigit) {
+            for (int i = 0; i < this.minNumberDigit; i++) {
+                this.digits.append(getNextDigitChar());
+            }
+        }
+    }
+
+    private void generatePunctuations() {
+        if (this.allowedPunctuation) {
+            for (int i = 0; i < this.minNumberPunctuation; i++) {
+                this.punctuations.append(getNextPunctuationChar());
+            }
+        }
+    }
+
     private char getNextChar() {
-        boolean[] possibility = {allowedLower, allowedUpper, allowedDigit, allowedPunctuation};
+        boolean[] possibility = { this.allowedLower, this.allowedUpper, this.allowedDigit, this.allowedPunctuation };
         Random random = new Random();
         int i;
 
@@ -99,22 +199,22 @@ public class PasswordGenerator {
 
     private char getNextLowerChar() {
         Random random = new Random();
-        return LOWER.charAt(random.nextInt(LOWER.length()));
+        return this.LOWER.charAt(random.nextInt(this.LOWER.length()));
     }
 
     private char getNextUpperChar() {
         Random random = new Random();
-        return UPPER.charAt(random.nextInt(UPPER.length()));
+        return this.UPPER.charAt(random.nextInt(this.UPPER.length()));
     }
 
     private char getNextDigitChar() {
         Random random = new Random();
-        return DIGITS.charAt(random.nextInt(DIGITS.length()));
+        return this.DIGITS.charAt(random.nextInt(this.DIGITS.length()));
     }
 
     private char getNextPunctuationChar() {
         Random random = new Random();
-        return PUNCTUATION.charAt(random.nextInt(PUNCTUATION.length()));
+        return this.PUNCTUATION.charAt(random.nextInt(this.PUNCTUATION.length()));
     }
 
 }
