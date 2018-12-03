a7afac585421faa97d110cefaae27cb88b8c00e0
Kiss Norbert <nn@norbilaptop.lan>
2018-12-03 18:57:20 +0100

    develope

---
+++ src/threads/countdownTimer/CountdownTimer.java
@@ -0,0 +1,33 @@
+package threads.countdownTimer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CountdownTimer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Countdown Timer");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainpane.fxml"));
+        Parent root = loader.load();
+//        MainViewController controller = loader.getController();
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+//        primaryStage.setOnHidden(e -> {
+//            controller.shutdown();
+//            Platform.exit();
+//        });
+        primaryStage.setResizable(true);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
